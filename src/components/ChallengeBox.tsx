a89988980d795710856e1306b3195de1507c69c0
Mateus Santos <m.santos.azeve@gmail.com>
2021-02-24 17:45:19 -0300

    dynamic page with useContext calls

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -4,33 +4,35 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const contextData = useContext(ChallengesContext);
-  
-  console.log(contextData);
-  
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
   
   return (
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src="icons/body.svg"/>
+            <img src={`icons/${activeChallenge.type}.svg`}/>
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma camihada de 3 minutos</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
             <button
               type="button"
               className={styles.challengeFailedButton}
-            >Falhei</button>
+              onClick={resetChallenge}
+            >
+              Falhei
+            </button>
+
             <button
               type="button"
               className={styles.challengeSucceededButton}
-            >Completei</button>
+            >
+              Completei
+            </button>
           </footer>
         </div>
       ) : (
